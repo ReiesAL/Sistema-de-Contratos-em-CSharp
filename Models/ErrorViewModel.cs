0f7ae4dc32d907a04791e8fa9f2c8f8c09e01184
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-06 11:10:03 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaContratos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
