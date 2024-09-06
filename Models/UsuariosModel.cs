402cc8755a01430f5a4cf34eabd4b4070bd418c5
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-06 16:22:52 -0300

    feat: Adicionado model para construção de usuários

---
+++ Models/UsuariosModel.cs
@@ -0,0 +1,23 @@
+﻿namespace SistemaContratos.Models
+{
+    public class UsuariosModel
+    {
+        public int Id { get; set; }
+        public int Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Data_criacao { get; set; }
+        public string Data_exclusao { get; set; }
+
+        // Construtor de usuarios
+        public UsuariosModel(int id, int cpf, string nome, string email, string dataCriacao, string dataExclusao)
+        {
+            Id = id;
+            Cpf = cpf;
+            Nome = nome;
+            Email = email;
+            Data_criacao = dataCriacao;
+            Data_exclusao = dataExclusao;
+        }
+    }
+}
