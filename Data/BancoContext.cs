6e4b29818df23e23a5241b7de6182d6fc1b9ad4e
Vinicius Reies <viniciusreies019@gmail.com>
2024-09-06 22:03:05 -0300

    feat: Criado classe BancContext

---
+++ Data/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaContratos.Models;
+
+namespace SistemaContratos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<ContratoModel> Contratos { get; set; }
+        public DbSet<UsuariosModel> Usuarios { get; set; }
+
+    }
+}
